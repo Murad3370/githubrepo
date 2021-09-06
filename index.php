f6099e59cd5f251d6703d0403ecddae067bb6d92
Murad3370 <75378471+Murad3370@users.noreply.github.com>
2021-09-06 16:15:36 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="" content="">
+    <title></title>
+  </head>
+  <body style="background:yellow">
+  <form action="main.php" method="post">
+	<input name="userid" type="text" placeholder="write your user name" />
+<br/>
+	<input name="Emailaddress" type="email" placeholder="write your email" /><br/>
+	<input name="btn" type="submit" />
+</form>
+  </body>
+  
+  
+  </html>
